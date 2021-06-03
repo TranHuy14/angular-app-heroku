32649d039a03b98c8861f6fe76dd97559cd443b1
Tran Huy <congtubuon9xxx@gmail.com>
2021-06-04 01:06:19 +0700

    update name

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/shop-huy-tran'));
+app.use(express.static('./dist/ShopHuyTran'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/shop-huy-tran/'}),
+    res.sendFile('index.html', {root: 'dist/ShopHuyTran/'}),
 );
 
 // Start the app by listening on the default Heroku port
